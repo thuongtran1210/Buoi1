67e8a4b8d96ba6d17d5cdac315f64d4b1c9b489a
thuongtran1210 <71269288+thuongtran1210@users.noreply.github.com>
2020-09-19 17:09:10 +0700

    Add files via upload

---
+++ bai1234567.py
@@ -0,0 +1,32 @@
+#Bài 1
+print("Hello Python")
+
+#Bài 2
+print("Trần Quang Thường")
+
+#Bài 3
+print(" Họ tên: Trần Quang Thường\n MSSV: 1906020158\n Khoa: Công nghệ thông tin\n Mã lớp: 13THC\n Chuyên Nghành: Lập Trình")
+
+#Bài 4
+a = 19
+b = 2001
+print("Kết quả a + b là: " +str(a+b))
+
+#Bài 5
+print("***********")
+print("***********")
+print("***********")
+print("***********")
+
+#Bài 6
+print("***********")
+print("*         *")
+print("*         *")
+print("***********")
+
+#Bài 7
+print("           *                ")
+print("          ****               ")
+print("         ******            ")
+print("        ********        ")
+print("       **********      ")
